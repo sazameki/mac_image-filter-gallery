8ca621da1788fdcccd82eb01fd2cc2a9d80c350d
Satoshi Numata <info@sazameki.jp>
2013-06-22 07:15:46 +0900

    Initial Commit

---
+++ ImageFilter Gallery/IGDocument.h
@@ -0,0 +1,13 @@
+//
+//  IGDocument.h
+//  ImageFilter Gallery
+//
+//  Created by numata on 2013/06/22.
+//  Copyright (c) 2013年 Satoshi Numata. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IGDocument : NSDocument
+
+@end
